62f9e41551b461964cd0da9c15d42657e6d35ec7
Wesley <sindewesley5@gmail.com>
2022-02-16 22:03:02 +0300

    Signed-off-by: Wesley <sindewesley5@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,30 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12" id="shops">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form class="flex items-center justify-center"
-                  v-on:submit.prevent="fetchShops">
-                <x-input class="py-4 px-6 w-1/2"
-                         placeholder="Find a shop near you"
-                         v-model="shopName" />
-                <x-button class="ml-4 py-4">Search</x-button>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <form class="flex items-center justify-center" v-on:submit.prevent="fetchShops">
+                <x-input class="w-1/2 px-6 py-4" placeholder="Find a shop near you" v-model="shopName" />
+                <x-button class="py-4 ml-4">Search</x-button>
             </form>
 
             <div class="mt-8 shadow-sm sm:rounded-lg">
                 <div v-show="locationErrorMessage" class="text-center">@{{ locationErrorMessage }}</div>
                 <div v-show="loading" class="text-center">Loading...</div>
                 <div v-show="!loading" class="grid grid-cols-3 gap-4" style="display: none;">
-                    <div class="p-6 bg-white border-b border-gray-200"
-                         v-for="shop in shops"
-                         :key="shop.id">
+                    <div class="p-6 bg-white border-b border-gray-200" v-for="shop in shops" :key="shop.id">
                         <div class="text-xl">@{{ shop.name }}</div>
-                        <div class="mt-4 text-gray-500"
-                             v-if="shop.distance">@{{ parseInt(shop.distance).toLocaleString() }}m away</div>
+                        <div class="mt-4 text-gray-500" v-if="shop.distance">@{{ parseInt(shop.distance).toLocaleString() }}m away</div>
                     </div>
                 </div>
             </div>
@@ -75,7 +69,7 @@
                                     this.locationErrorMessage = "Please allow location access.";
                                 }
                             });
-                        } else { 
+                        } else {
                             x.innerHTML = "Geolocation is not supported by this browser.";
                         }
                     },
@@ -88,4 +82,6 @@
             }).mount('#shops');
         </script>
     @endpush
+    <footer class="px-3 text-lg text-center text-white no-underline uppercase bg-gray-900 py- italilc"><a
+            href="https://www.wesley.io.ke/">Wesley Sinde: Some rights reserved {{ now()->year }}</a></footer>
 </x-app-layout>
